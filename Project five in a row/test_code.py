0686679481d83263f5ae0c9b8f1851f7c51a4ddb
HengruiCui <46952293+HengruiCui@users.noreply.github.com>
2019-04-25 20:03:51 -0400

    Add files via upload

---
+++ Project five in a row/test_code.py
@@ -0,0 +1,13 @@
+from AI import WinBoard
+from AI import Click
+from AI import Check
+from AI import AIcompute
+from AI import playercompute
+from AI import score
+from AI import AIput
+from AI import computer
+
+
+print('ok')
+
+
